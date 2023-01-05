5aac23dada93625112cfd8b9032af26859670129
Diana <dju.diana@gmail.ru>
2023-01-05 18:45:52 +0300

    Решение задач

---
+++ Lesson1/Program.cs
@@ -0,0 +1,89 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+//Console.WriteLine("Введите число");
+//int number1=Convert.ToInt32 (Console.ReadLine());
+
+//Console.WriteLine("Введите число");
+//int number2=Convert.ToInt32 (Console.ReadLine());
+
+//if (number1>number2)
+//{
+    //Console.WriteLine(number1);
+//}
+//else
+//{
+  //  Console.WriteLine(number2);
+//}
+
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+//Console.WriteLine("Введите число");
+//int number1=Convert.ToInt32 (Console.ReadLine());
+
+//Console.WriteLine("Введите число");
+//int number2=Convert.ToInt32 (Console.ReadLine());
+
+//Console.WriteLine("Введите число");
+//int number3=Convert.ToInt32 (Console.ReadLine());
+
+//int max=number1;
+
+//if (number1>max)
+//{
+    //max=number1;
+//}
+//if (number2>max)
+//{
+  //  max=number2;
+//}
+//if (number3>max)
+//{
+  //  max=number3;
+//}
+//Console.Write("max= ");
+//Console.WriteLine(max);
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+//Console.WriteLine("Введите число");
+//int number1=Convert.ToInt32 (Console.ReadLine());
+
+//if (number1%2==0)
+//{
+  //  Console.WriteLine("Да");
+//}
+//else
+//{
+  //  Console.WriteLine("Нет");
+//}
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+//Console.WriteLine("Введите число");
+//int number1=Convert.ToInt32 (Console.ReadLine());
+//
+//int number2=1;
+//
+//while(number2<=number1)
+//{
+//if (number2%2==0)
+//{
+//    Console.WriteLine(number2);
+//number2+=1;
+//}
+//}
